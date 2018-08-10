c69ddfc500d5c2d6b3a135b656977d408379415e
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-08-10 12:53:02 +0100

    add mockNote and doubling title and body + pasing test for storing notes in @all_notes

--- spec/notepad_spec.rb
+++ spec/notepad_spec.rb
@@ -2,7 +2,7 @@ require 'notepad'
 
 describe Notepad do
   subject(:notepad) { Notepad.new } 
-  # let(:mockNote) { double :note, :title => 't1', :body => 'b1' }
+  let(:mockNote) { double :note, :title => 't1', :body => 'b1' }
 
   it 'initializes with an empty @all_notes hash' do
     expect(subject.all_notes).to be_empty
@@ -23,8 +23,7 @@ describe Notepad do
   it { is_expected.to respond_to(:add_note).with(1).argument }
 
   it 'stores notes in @all_notes' do 
-    n1 = Note.new("testTitle1", "testBody1") 
-    subject.add_note(n1)
+    expect(subject.add_note(mockNote)).not_to be_empty
   end
 
   # it 'stores note bodies in @all_bodies' do 
@@ -32,14 +31,6 @@ describe Notepad do
 
   # it 'stores note titles and bodies as a hash in @all_notes' do 
   # end 
-
-  it 'stores notes' do 
-    testNote1 = "hello"
-    subject.add_note(testNote1)
-    expect(subject.all_notes).not_to be_empty
-  end
-
-
 
 
 end
